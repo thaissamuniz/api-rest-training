340dedd250739f3b7ea8a791898ac8c71b0efe74
thaissa <thaiissa.muniiz@gmail.com>
2023-03-07 16:57:54 -0300

    add search for editora

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,6 +4,7 @@ import LivroController from "../controllers/livrosController.js";
 const router = express.Router();
 
 router.get("/livros", LivroController.listarLivros)
+    .get("/livros/busca", LivroController.listarLivroPorEditora)
     .get("/livros/:id", LivroController.listarLivroPorId)
     .post("/livros", LivroController.cadastrarLivro)
     .put("/livros/:id", LivroController.atualizarLivro)
